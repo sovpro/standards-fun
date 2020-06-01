46674cac729b59789930b154cc5c6c229242aec0
sovpro <sovpro@users.noreply.github.com>
2020-06-01 14:00:35 -0400

    Add package.json and npm start script for browser viewing

---
+++ README.md
@@ -0,0 +1,6 @@
+Experiments with W3C JavaScript Web APIs
+
+```bash
+npm ci
+npm start
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+console.log (`
+To view run the following:
+
+  cd ${__dirname}
+
+  npm ci
+
+  npm start
+`)
