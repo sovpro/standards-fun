453f0ad703376f96919f20435079ba7583e32f9c
sovpro <sovpro@users.noreply.github.com>
2020-06-12 02:57:23 -0400

    Make options to makelogfunc explicit
    Add JSDoc style comment

--- javascripts/makelogfunc.js
+++ javascripts/makelogfunc.js
@@ -1,10 +1,30 @@
-// Utility for logging text in the DOM
-// make log func for a given configuration
-// {
-//   [insertBefore | insertAfter | appendChild]: selector,
-//   head: boolean,
-//   roll: number
-// }
+/**
+ * Utility for generating a function to
+ * log text in the DOM.
+ *
+ * @param {object} [options] options object
+ * @param {string} [options.position=beforeend] where to
+ *     position the log relative to selector. One of beforebegin,
+ *     afterbegin, beforeend, afterend.
+ * @param {string} [options.selector=body] selector to
+ *     position log element relative to.
+ * @param {string} [options.from=end] where new lines
+ *     should appear from in log. one of: start, end.
+ * @param {number} [options.rotate=100] rotate out old
+ *     lines after this number of logs. 0 disables.
+ *
+ * @example
+ * const logToElem = makelogfunc ({
+ *   position: 'beforeend',
+ *   selector: '#log-elem',
+ *   from: 'start',
+ *   rotate: 10
+ * })
+ * logToElem ('Hello')
+ * logToElem ('World')
+ *
+ * @returns {Function} DOM logging function
+ */
 const makelogfunc = (() => {
     // library function for all log funcs
     const makeline = (text) => {
@@ -12,36 +32,38 @@ const makelogfunc = (() => {
         line.appendChild(document.createTextNode(text))
         return line
     }
-    return (config = {'beforeend':'body', 'head':false, 'roll':100}) => {
+    return (config = {position: 'beforeend', selector: 'body', from: 'end', rotate: 100}) => {
         let _queue = []
         let _elem = undefined
         let _log = undefined
+        const head = config.from === 'start'
         const log = (text) => {
-            if (_elem.childElementCount === config.roll)
-                _elem.removeChild(config.head ? _elem.lastElementChild : _elem.firstElementChild)
+            if (config.rotate > 0)
+              while (_elem.childElementCount >= config.rotate)
+                _elem.removeChild(head ? _elem.lastElementChild : _elem.firstElementChild)
             _elem.insertAdjacentElement(
-                config.head ? 'afterbegin' : 'beforeend',
+                head ? 'afterbegin' : 'beforeend',
                 makeline(text))
         }
         const loaded = () => {
-            const position = Object.getOwnPropertyNames(config).
-                                    find(name => /^(before|after)(begin|end)$/.test(name))
-            const selector = config[position]
+            const position = config.position
+            const selector = config.selector
             _elem = document.querySelector(selector).insertAdjacentElement(
                 position, document.createElement('ol'))
-            if (config.head) _elem.reversed = true
+            if (head) _elem.reversed = true
             let text = undefined
             console.log(_queue)
-            while (text = _queue[config.head ? 'pop' : 'shift']()) log(text)
+            while (text = _queue[head ? 'pop' : 'shift']()) log(text)
             _log = log
         }
         if (document.readyState !== 'complete') {
             document.addEventListener(
                 'DOMContentLoaded', () => loaded(), { once: true })
             const _enqueue = (text) => {
-                if (_queue.length === config.roll)
-                    _queue[config.head ? 'pop' : 'shift']()
-                _queue[config.head ? 'unshift' : 'push'](text)
+                if (config.rotate > 0)
+                  while (_queue.length >= config.rotate)
+                    _queue[head ? 'pop' : 'shift']()
+                _queue[head ? 'unshift' : 'push'](text)
             }
             _log = _enqueue
         }
